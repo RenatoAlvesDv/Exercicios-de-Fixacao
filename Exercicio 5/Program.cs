55a1891987ba30f07dc6bd1a52bb93ba4a1624a2
Your Name <renato_inovacao@hotmail.com>
2020-11-29 21:40:12 -0300

    Finalizado

---
+++ Exercicio 5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Excercicio_5_de_segunda_feira
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercício 5 de Segunda-feira 23//11
+    
+            Console.WriteLine("Tabuada do 1 até 10 !");
+
+            for (var e = 1; e <= 10; e++)   //Primeiro laço executa
+            {
+                for (var d = 1; d <= 10; d++)
+                {
+                    Console.WriteLine(+e+ " x " +d+ " = " +e * d);              
+                }                        
+             Console.WriteLine();
+            }
+
+           
+        }
+    }
+}
